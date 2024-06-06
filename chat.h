e72f52c23fe1e126f89221506dd296e36d919065
Danielg220 <135673669+Danielg220@users.noreply.github.com>
2024-06-06 13:48:52 -0300

    dani atr

--- chat.h
+++ chat.h
@@ -1,1 +1,16 @@
 // Daniel
+
+#ifndef CHAT_H
+#endif CHAT_H
+
+#include "mensaje.h"
+#include "DataTypes/fecha.h"
+
+class chat{
+  private:
+    Fecha Fecha;
+    int Hora;
+    int CantMensajes
+  public:
+  
+};
