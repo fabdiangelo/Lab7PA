53fc37e606903794d9fcfee1022f9df875522673
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-06-06 13:55:44 -0300

    Update administrador.h

--- administrador.h
+++ administrador.h
@@ -1,1 +1,13 @@
-// Bernardo
+#ifndef ADMINISTRADOR_H
+#define ADMINISTRADOR_H
+
+#include <iostream>
+#include "usuario.h"
+
+class administrador : public usuario{
+	public:
+		administrador(string, string);
+		~administrador();
+};
+
+#endif
