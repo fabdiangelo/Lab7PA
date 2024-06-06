5f655ad84433d69c4209bdb367aa67b3f679a623
Danielg220 <135673669+Danielg220@users.noreply.github.com>
2024-06-06 14:41:59 -0300

    palusa

--- departamento.h
+++ departamento.h
@@ -1,1 +1,24 @@
 // Daniel
+#ifndef DEPARTAMENTO_H
+#define DEPARTAMENTO_H
+
+#include "zona.h"
+#include "ICollection/collections/List.h"
+
+class departamento {
+  private:
+    string nombre;
+    char id;
+    List* zona;
+  public:
+    departamento(string nombre, char id);
+    string getNombre();
+    char getId();
+    void setNombre(string);
+    void setId(char);
+    List* obtenerListado();
+    zona seleccionarZona(string);
+    ~departamento();
+}
+
+#endif DEPARTAMENTO_H
