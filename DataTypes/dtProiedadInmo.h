d5cccdfd64fe78bf0081b82dadaf6ef78c67ab4f
Fabricio <fuentesfabricio17@gmail.com>
2024-06-06 14:59:02 -0300

    propInmo

---
+++ DataTypes/dtProiedadInmo.h
@@ -0,0 +1,28 @@
+#ifndef DTPROPIEDADINMO_H
+#define DTPROPIEDADINMO_H
+
+#include <iostream>
+#include "DataTypes/direccion.h"
+
+using namespace std;
+
+class dtProiedadInmo
+{
+private:
+    char * codigo;
+    string nombreInmo;
+    int cantAmbientes, cantDormitorios, cantBanios, m2Edificados;
+    bool garage, enVenta, enAlquiler;
+    direccion * direccion, direccionInmo;
+public:
+    dtProiedadInmo(char* codigo, int cantAmbientes, int cantDormitorios, int cantBanios, bool garage, direccion* direccion);
+    ~dtProiedadInmo();
+};
+
+dtProiedadInmo::dtProiedadInmo(/* args */)
+{
+}
+
+dtProiedadInmo::~dtProiedadInmo()
+{
+}
