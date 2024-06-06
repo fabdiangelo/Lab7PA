3fdd25b239df78464ce04510659ea7263414be63
Danielg220 <135673669+Danielg220@users.noreply.github.com>
2024-06-06 14:57:23 -0300

    sin terminar

--- edificio.h
+++ edificio.h
@@ -1,1 +1,18 @@
 // Daniel
+#ifndef EDIFICIO_H
+#define EDIFICIO_H
+
+#include "apartamento.h"
+
+class edificio{
+  private:
+    string nombre;
+    int cantPisos;
+    int gastosComunes;
+  public:
+    string getNombre();
+    int getCantPisos();
+    int getGasto();
+}
+
+#endif EDIFICIO_H
