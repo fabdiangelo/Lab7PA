6827cb1285feccae82b4e1ba6ab01fd90d8eaeca
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-06-06 14:41:34 -0300

    Update casa.cpp

--- casa.cpp
+++ casa.cpp
@@ -1,1 +1,11 @@
+#include "casa.h"
 
+casa::casa(string codigo, int cantAmbientes, int cantDormitorios, int cantBanios, bool garage, direccion* direccion, int m2Edificados, int precioAlquiler, int precioVenta, int mt2, int mv2)::propiedad(codigo, cantAmbientes, cantDormitorios, cantBanios, garage, direccion, m2Edificados, precioAlquiler, precioVenta){
+    this->M2T = mt2;
+    this->M2V = mt2;
+    //NOSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+}
+
+casa::~casa(){
+    cout << "derrumbo...";
+}
