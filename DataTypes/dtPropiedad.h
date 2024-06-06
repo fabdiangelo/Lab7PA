d8c1be0b261b05210074d7edec683ef0ba947147
Chizzyjpg <chizzyjpg@gmail.com>
2024-06-06 12:42:45 -0300

    no hab'ia subido na brro
    xd

--- DataTypes/dtPropiedad.h
+++ DataTypes/dtPropiedad.h
@@ -1,1 +1,19 @@
 // Daniel
+#ifndef DTPROPIEDAD_H
+#define DTPROPIEDAD_H
+#include <iostream>
+
+using namespace std;
+
+class dtPropiedad
+{
+private:
+
+public:
+    dtPropiedad();
+    ~dtPropiedad();
+
+};
+
+
+#endif
