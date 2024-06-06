68d6a313e9b3a553cd20db847efef74f25b9ca26
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-06-06 13:38:57 -0300

    Update casa.h

--- casa.h
+++ casa.h
@@ -1,1 +1,20 @@
-// Bernardo
+#ifndef CASA_H
+#define CASA_H
+
+#include <iostream>
+#include propiedad.h
+
+class casa : public propiedad{
+	private:
+    int M2Verdes;
+		int M2Totales;
+	public:
+		casa(string, int, int, int, bool, direccion, int, int, int, chat**,int, int);
+		int getM2Verdes();
+    int getM2Totales();
+    void setM2Verdes(int);
+		void setM2Totales(int);
+		~casa();
+}
+
+#endif
