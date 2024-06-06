3cc3acfa21ecd2a2032e090a18a7a1fefe68b5a8
Danielg220 <135673669+Danielg220@users.noreply.github.com>
2024-06-06 14:11:50 -0300

    creo que hay problema en listado de mensaje

--- chat.h
+++ chat.h
@@ -1,16 +1,28 @@
 // Daniel
 
 #ifndef CHAT_H
-#endif CHAT_H
+#define CHAT_H
 
 #include "mensaje.h"
 #include "DataTypes/fecha.h"
-
+#include "DataTypes/registroMensajes.h"
 class chat{
   private:
-    Fecha Fecha;
+    fecha Fecha;
     int Hora;
-    int CantMensajes
+    int CantMensajes;
+    MisMensajes* mensaje; //para marcar que el chat tiene una lista de mensajes...mal creo
   public:
-  
+    chat(fecha Fecha,int Hora,int CantMensajes);
+    fecha getFecha();
+    int getHora();
+    int getCantMensajes();
+    int getInteresado();
+    void setCantMensajes(int);
+    void BorrarMensajes();
+    registroMensajes ObtenerRegistro();
+    void CrearMensaje(string);
+    ~chat();
 };
+
+#endif CHAT_H
