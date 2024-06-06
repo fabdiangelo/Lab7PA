453e227cfc303158daa2539efb5408d692edeb50
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-06-06 14:25:48 -0300

    Update apartamento.cpp

--- apartamento.cpp
+++ apartamento.cpp
@@ -1,1 +1,7 @@
+#include "apartamento.h"
 
+
+apartamento(string, int, int, int, bool, direccion, int, int, int, chat**, int){
+    this->correo = corre;
+    this->contrasenia = contra;//NOSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+}
