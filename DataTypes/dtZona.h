85a2040143f1360760618dca68f547686a3b11a1
Fabricio <fuentesfabricio17@gmail.com>
2024-06-06 12:45:42 -0300

    dtRespuesta y dtZona

--- DataTypes/dtZona.h
+++ DataTypes/dtZona.h
@@ -1,1 +1,19 @@
-// Fabricio
+#ifndef DTZONA_H
+#define DTZONA_H
+
+#include <iostream>
+
+using namespace std;
+
+class dtZona
+{
+private:
+    string nombre, codigo;
+public:
+    dtZona(string nombre, string codigo);
+    ~dtZona();
+    string getNombre();
+    string getCodigo();
+};
+
+#endif
