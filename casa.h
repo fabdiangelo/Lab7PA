0c03c2acd9a72c74f6f5afab68ed417b6c8d6c6e
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-06-06 13:40:18 -0300

    Update casa.h

--- casa.h
+++ casa.h
@@ -6,13 +6,13 @@
 
 class casa : public propiedad{
 	private:
-    int M2Verdes;
+    		int M2Verdes;
 		int M2Totales;
 	public:
 		casa(string, int, int, int, bool, direccion, int, int, int, chat**,int, int);
 		int getM2Verdes();
-    int getM2Totales();
-    void setM2Verdes(int);
+    		int getM2Totales();
+    		void setM2Verdes(int);
 		void setM2Totales(int);
 		~casa();
 }
