f8a157b01221d3004b2c3fa08afe9bbb7447ee1a
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-06-06 13:35:38 -0300

    Update apartamento.h

--- apartamento.h
+++ apartamento.h
@@ -1,1 +1,11 @@
-// Bernardo
+#include propiedad.h
+
+class apartamento : public propiedad{
+	private:
+		int M2Totales;
+	public:
+		apartamento(string, int, int, int, bool, direccion, int, int, int, chat**, int);
+		int getM2Totales();
+		void setM2Totales(int);
+		~apartamento();
+}
