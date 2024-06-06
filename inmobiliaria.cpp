78c37a81a865bd853c94a6b674ab7cc983728bf2
Fabricio <78382080+fabdiangelo@users.noreply.github.com>
2024-06-06 10:55:03 -0300

    inmobiliaria

--- inmobiliaria.cpp
+++ inmobiliaria.cpp
@@ -1,27 +1,48 @@
 #include "inmobiliaria.h"
 
 // CONSTRUCTOR:
-inmobiliaria::inmobiliaria(string nom, direccion* dir)
-{
+inmobiliaria::inmobiliaria(string nom, direccion* dir){
     this -> nombre = nom;
     this -> direccion = dir;
 }
 
 // DESTRUCTOR:
-inmobiliaria::~inmobiliaria()
-{
+inmobiliaria::~inmobiliaria(){
 }
 
 // GETTERS:
-string inmobiliaria::getNombre()
-{
+string inmobiliaria::getNombre(){
     return this -> nombre;
 }
 
-direccion * inmobiliaria::getDireccion()
+direccion* inmobiliaria::getDireccion()
 {
     return this -> direccion;
 }
 
+// SETTERS:
+void inmobiliaria::setNombre(string nom){
+    this -> nombre = nom;
+}
+
+void inmobiliaria::setDireccion(direccion* dir){
+    this -> direccion = dir;
+}
+
 // FUNCIONALIDAD:
 //pendiente...
+propiedad IngresarDatosApartamento(int cantAmb, int cantDorm, int cantBa, bool garage, direccion* dir, int m2);
+
+propiedad IngresarDatosCasa(int cantAmb, int cantDorm, int cantBa, bool garage, direccion* dir, int m2Edif, int m2Verd);
+
+void IngresarPrecioAlquiler(int precio);
+
+void IngresarPrecioVenta(int precio);
+
+void BorrarPropiedad(string prop);
+
+propiedad ModificarDatosApartamento(int cantAmb, int cantDorm, int cantBa, bool garage, direccion* dir, int m2);
+
+propiedad ModifiarDatosCasa(int cantAmb, int cantDorm, int cantBa, bool garage, direccion* dir, int m2Edif, int m2Verd);
+
+void MostrarDatos();
