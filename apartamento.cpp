8f1ef4e2d8d6442e4f96b09226bc529077485878
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-06-09 11:51:22 -0300

    Update apartamento.cpp

--- apartamento.cpp
+++ apartamento.cpp
@@ -3,9 +3,15 @@
 
 apartamento::apartamento(string codigo, int cantAmbientes, int cantDormitorios, int cantBanios, bool garage, direccion* direccion, int m2Edificados, int precioAlquiler, int precioVenta, int mt2)::propiedad(codigo, cantAmbientes, cantDormitorios, cantBanios, garage, direccion, m2Edificados, precioAlquiler, precioVenta){
     this->M2T = mt2;
-    //NOSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 }
 
+void apartamento::setM2Totales(int mt2){
+    this->M2T = mt2;
+}
+
+int apartamento::getM2Totales(){
+    return this->M2T;
+}
 apartamento::~apartamento(){
-    cout << "derrumbo...";
+    cout << "incendio...";
 }
