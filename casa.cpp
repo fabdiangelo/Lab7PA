c312cfc0d0271a6958da539e2586c500e859c6f4
Bernardo776 <141965250+Bernardo776@users.noreply.github.com>
2024-06-09 11:55:50 -0300

    Update casa.cpp

--- casa.cpp
+++ casa.cpp
@@ -1,9 +1,25 @@
 #include "casa.h"
 
 casa::casa(string codigo, int cantAmbientes, int cantDormitorios, int cantBanios, bool garage, direccion* direccion, int m2Edificados, int precioAlquiler, int precioVenta, int mt2, int mv2)::propiedad(codigo, cantAmbientes, cantDormitorios, cantBanios, garage, direccion, m2Edificados, precioAlquiler, precioVenta){
-    this->M2T = mt2;
-    this->M2V = mt2;
+    this->M2Totales = mt2;
+    this->M2Verdes = mt2;
     //NOSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+}
+
+void casa::setM2Totales(int mt2){
+    this->M2Totales = mt2;
+}
+
+void casa::setM2Verdes(int mtv){
+    this->M2Verdes = mtv;
+}
+
+int casa::getM2Totales(){
+    return this->M2Totales;
+}
+
+int casa::getM2Verdes(){
+    return this->M2Verdes;
 }
 
 casa::~casa(){
