c4ca54585faa5e73d400206ac2973b4f56110dfe
chizzyjpg <64444067+chizzyjpg@users.noreply.github.com>
2024-06-10 11:57:58 -0300

    Update sistema.cpp

--- sistema.cpp
+++ sistema.cpp
@@ -20,64 +20,64 @@ void sistema::cerrarSesion(){
 
 }
 
-dtRespuesta sistema::ingresarInmobiliaria(string correo, direccion * dir, string nombre){
-    
+dtRespuesta* sistema::ingresarInmobiliaria(string correo, direccion * dir, string nombre){
+    return NULL;    
 }
 
-dtRespuesta sistema::darAlta(string nombre, string apellido, int edad, string email){
-
+dtRespuesta* sistema::darAlta(string nombre, string apellido, int edad, string email){
+    return NULL;
 }
 
-dtRespuesta sistema::ingresarEdificio(string nombre, int pisos, int gastosComunes){
-
+dtRespuesta* sistema::ingresarEdificio(string nombre, int pisos, int gastosComunes){
+    return NULL;
 }
 
-List sistema::listarDepartamentos(){
-
+List* sistema::listarDepartamentos(){
+    return NULL;
 }
 
-List sistema::listarZonaDep(char depSeleccionado){
-
+List* sistema::listarZonaDep(char depSeleccionado){
+    return NULL;
 }
 
 string sistema::seleccionarZona(string zonaSeleccionada){
-
+    return NULL;
 }
 
-List sistema::listarEdificios(){
-
+List* sistema::listarEdificios(){
+    return NULL;
 }
 
 void sistema::seleccionarEdificio(){
 
 }
 
-dtRespuesta sistema::ingresarDatosApartamento(int ambientes, int dormitorios, int banios, bool garage, direccion * dir, int metros){
-
+dtRespuesta* sistema::ingresarDatosApartamento(int ambientes, int dormitorios, int banios, bool garage, direccion * dir, int metros){
+    return NULL;
 }
 
-dtRespuesta sistema::ingresarDatosCasa(int ambientes, int dormitorios, int banios , bool garage, direccion * dir, int M2Verde){
-
+dtRespuesta* sistema::ingresarDatosCasa(int ambientes, int dormitorios, int banios , bool garage, direccion * dir, int M2Verde){
+    return NULL;
 }
 
-dtRespuesta sistema::ingresarPrecioAlquiler(int valorAlqui){
-
+dtRespuesta* sistema::ingresarPrecioAlquiler(int valorAlqui){
+    return NULL;
 }
 
-dtRespuesta sistema::ingresarPrecioVenta(int valorVenta){
-
+dtRespuesta* sistema::ingresarPrecioVenta(int valorVenta){
+    return NULL;
 }
 
 void sistema::finalizarAlta(){
 
 }
 
-List sistema::listarPropiedades(string zonaSeleccionada){
-
+List* sistema::listarPropiedades(string zonaSeleccionada){
+    return NULL;
 }
 
-dtPropiedadInmo sistema::infoPropInmo(string propInmo){
-
+dtPropiedadInmo* sistema::infoPropInmo(string propInmo){
+    return NULL;
 }
 
 void sistema::ingresarCodigoProp(string codigo){
@@ -88,30 +88,30 @@ void sistema::borrarPorp(string codigo){
 
 }
 
-List sistema::listarPropMens(string zonaSeleccionada){
-
+List* sistema::listarPropMens(string zonaSeleccionada){
+    return NULL;
 }
 
-registroMensajes sistema::listarMensajes(string propSeleccionada){
-
+registroMensajes* sistema::listarMensajes(string propSeleccionada){
+    return NULL;
 }
 
 void sistema::crearChat(){
 
 }
 
-dtRespuesta sistema::ingresarMensaje(string mensaje){
-
+dtRespuesta* sistema::ingresarMensaje(string mensaje){
+    return NULL;
 }
 
-List sistema::listarMensajesRecientes(){
-
+List* sistema::listarMensajesRecientes(){
+    return NULL;
 }
 
-dtInmobiliaria sistema::mostrarDatosInmo(){
-
+dtInmobiliaria* sistema::mostrarDatosInmo(){
+    return NULL;
 }
 
-dtPropiedad sistema::mostrarPropiedad(){
-
+dtPropiedad* sistema::mostrarPropiedad(){
+    return NULL;
 }
